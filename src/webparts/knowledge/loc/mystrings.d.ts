07662fce828bee401d688070c4b043af08619474
munkieman <jason_mc@btinternet.com>
2019-08-10 15:31:22 +0100

    updated core files

---
+++ src/webparts/knowledge/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IKnowledgeWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'KnowledgeWebPartStrings' {
+  const strings: IKnowledgeWebPartStrings;
+  export = strings;
+}
